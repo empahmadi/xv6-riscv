62e4a5ec80ec32fc82194879c551dac267679209
empahmadi <emp.ahmadi@gmail.com>
2023-04-04 16:06:52 +0330

    creating a test for hello system call and introduce it to Makefile file.

---
+++ user/helloTest.c
@@ -0,0 +1,13 @@
+//
+// Created by empahmadi on 4/4/23.
+//
+
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "user.h"
+
+int main(void){
+    hello();
+    printf("test passed successfully\n");
+    exit(0);
+}
