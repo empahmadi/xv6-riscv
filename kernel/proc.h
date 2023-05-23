0699beeb3c9e91524b02d68b9c29b3af10d8be0d
negrsm <irannsm@yahoo.com>
2023-05-23 00:02:44 -0700

    adding variables in proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -91,6 +91,10 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int ttime;                   //termination time
+  int rutime;                  //running time
+  int retime;                  //ready time
+  int stime;                   //sleeping time
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
